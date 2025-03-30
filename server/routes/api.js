733f0c1f589627bd9dfd2f6f4c92de91638d4eff
GeebergyStatic <staticbusiness1411@gmail.com>
2025-03-30 02:28:39 +0100

    Update email

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 // my-app/server/routes/api.js
 const express = require('express');
+const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
 const router = express.Router();
 const User = require('../model');
@@ -1474,6 +1475,36 @@ router.get('/fetch-minted-nfts/:userId', async (req, res) => {
   } catch (error) {
     console.error('Error retrieving NFTs:', error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/send-email', async (req, res) => {
+  const { customName, customEmail, customMessage } = req.body;
+
+  // Set up Gmail SMTP transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'obeingilbert3884@gmail.com', // Your Gmail address
+      pass: process.env.gmail_pass,   // Your Gmail password (or App password if 2FA enabled)
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: customEmail,
+    to: 'oremifoundation.ng@gmail.com',  // Your email where you want to receive the message
+    subject: `Message from ${customName}`,
+    text: `Message from: ${customName}\nEmail: ${customEmail}\n\nMessage:\n${customMessage}`,
+  };
+
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
   }
 });
 
